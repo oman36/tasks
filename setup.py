28f8de1809b40f1b86adc48181c9f0b21f0c16bc
Petrov Vladimir <neoman36@gmail.com>
2018-10-26 18:40:41 +0300

    Version was changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tasks',
-    version='0.5',
+    version='0.6',
     description='Manager for delayed tasks',
     url='http://github.com/oman36/tasks',
     author='Petrov Vladimir',
