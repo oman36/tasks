2aace07d3629f8c38d0af703dd3eb751728d49ed
Petrov Vladimir <neoman36@gmail.com>
2018-10-27 16:45:17 +0300

    0.6 was refactored 2

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 import os
+import shutil
 import time
 
 import jsonschema
@@ -10,10 +11,45 @@ from .exceptions import (
     FatalException,
     TaskNotExistException,
 )
-from .orm import Session, Task
+from .orm import globals_sessions, Task as TaskModel
 from .settings import SETTINGS
+from .email import send_email
 
-tasks_inited = dict()
+
+class InterpretedTask:
+    def __init__(self, name, callback, json_schema):
+        self.name = name
+        self.callback = callback
+        self.json_schema = json_schema or {}
+
+    def validate_params(self, params):
+        try:
+            jsonschema.validate(params, self.json_schema)
+        except jsonschema.ValidationError as er:
+            raise BadRequestException(er.__str__())
+
+    def run(self, **kwargs):
+        return self.callback(**kwargs)
+
+
+class InterpretedTaskList:
+    def __init__(self):
+        self.tasks_inited = dict()
+
+    def append(self, name, callback, json_schema=None):
+        if name in self.tasks_inited:
+            raise FatalException('Task "{}" already exists'.format(name))
+
+        self.tasks_inited[name] = InterpretedTask(name, callback, json_schema)
+
+    def get(self, name) -> InterpretedTask:
+        if name not in self.tasks_inited:
+            raise TaskNotExistException(name)
+
+        return self.tasks_inited[name]
+
+
+interpreted_task_list = InterpretedTaskList()
 
 
 def task(name, json_schema=None):
@@ -21,13 +57,8 @@ def task(name, json_schema=None):
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
 
-        if name in tasks_inited:
-            raise FatalException('Task "{}" already exists'.format(name))
+        interpreted_task_list.append(name, wrapper, json_schema)
 
-        tasks_inited[name] = {
-            'callback': wrapper,
-            'json_schema': dict() if json_schema is None else json_schema,
-        }
         return wrapper
 
     return decorator
@@ -71,40 +102,103 @@ class Result:
         return self.msg
 
 
-def run_command(args: dict, task_in_db=None):
-    if 'task_name' not in args:
-        raise BadRequestException('task_name is required')
+class Task:
+    def __init__(self, task_name, params=None, result_dir=None):
+        self.task_name = task_name
+        self.params = params or {}
+        self.result_dir = result_dir
+        self.validate()
 
-    if args['task_name'] not in tasks_inited:
-        raise TaskNotExistException(args['task_name'])
+    def validate(self):
+        self.get_interpreted_task().validate_params(self.params)
 
-    called_task = tasks_inited[args['task_name']]
+    def get_interpreted_task(self):
+        return interpreted_task_list.get(self.task_name)
 
-    try:
-        jsonschema.validate(args['params'], called_task['json_schema'])
-    except jsonschema.ValidationError as er:
-        raise BadRequestException(er.__str__())
+    def run(self):
+        result = self.get_interpreted_task().run(**self.params)
 
-    session = Session()
+        if isinstance(result, Result):
+            self.handle_result(result)
 
-    if args['task_name'] != 'runserver':
-        if task_in_db is None:
-            task_in_db = Task(
-                name=args['task_name'],
-                params=json.dumps(args['params']),
+        return result
+
+    def handle_result(self, result):
+        result_dir = self.result_dir or os.getcwd()
+
+        if not os.path.isdir(result_dir):
+            os.makedirs(result_dir)
+
+        for file_data in result.files:
+            try:
+                shutil.move(file_data['tmp_path'], os.path.join(result_dir, file_data['name']))
+            finally:
+                del file_data['tmp_path']
+
+
+class WebTask(Task):
+    def __init__(self, task_name_or_model, params=None, email=None):
+
+        if isinstance(task_name_or_model, str):
+
+            if task_name_or_model == 'runserver':
+                raise BadRequestException('Task name "runserver" is not allowed')
+
+            super().__init__(task_name_or_model, params)
+
+            self.model = TaskModel(
+                name=self.task_name,
+                params=json.dumps(self.params),
                 status='new',
             )
-            session.add(task_in_db)
-            session.commit()
+
+            if email is not None:
+                self.model.email = email
+
+            self.save_model()
+
         else:
-            session.add(task_in_db)
+            self.model = task_name_or_model  # type:TaskModel
+            self.task_name = self.model.name
+            self.params = json.loads(self.model.params)
 
-    result = called_task['callback'](**args['params'])
+    def handle_result(self, result):
+        if self.model.email:
+            return
 
-    if args['task_name'] != 'runserver':
-        task_in_db.status = 'finished'
-        task_in_db.result = str(result)
-        session.commit()
-    session.close()
+        result_dir = os.path.join(SETTINGS['files']['web_dir'], str(self.model.id))
+        files = []
 
-    return result
+        if not os.path.isdir(result_dir):
+            os.makedirs(result_dir)
+
+        for file_data in result.files:
+            file_name = file_data['name']
+            shutil.move(file_data['tmp_path'], os.path.join(result_dir, file_name))
+
+            files.append(file_name)
+
+        self.model.files = json.dumps(files)
+        self.save_model()
+
+    def run(self):
+        result = super().run()
+
+        self.model.status = 'finished'
+        self.model.result = str(result)
+
+        self.save_model()
+
+        if self.model.email:
+            send_email(
+                [self.model.email],
+                'Task {} was completed.'.format(self.task_name),
+                str(result),
+                result.files if isinstance(result, Result) else []
+            )
+
+        return result
+
+    def save_model(self):
+        globals_sessions[0].add(self.model)
+        globals_sessions[0].commit()
